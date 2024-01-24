21e265c4b05ea04da59b8906433ff21f596fb00c
Andrés Rodríguez <andrudeluxe@gmail.com>
2024-01-24 14:06:49 -0500

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        port: '',
+        pathname: '/**',
+      },
+    ]
+  }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,147 @@
-import Image from "next/image";
+import Link from 'next/link'
+import { GoogleIcon, navlist } from '../constants'
+import {
+  ChatBubbleOvalLeftIcon,
+  PaperAirplaneIcon,
+  CameraIcon,
+  UsersIcon,
+} from '@heroicons/react/24/outline'
+import Image from 'next/image'
 
 export default function Home() {
+  const currentHour = new Date().getHours()
+  const currentMinutes = new Date().getMinutes()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex justify-center min-h-screen relative">
+      <div className="gradient-03 z-0 xl:block hidden" />
+      <nav className="absolute md:flex w-full justify-center items-center z-20 hidden ">
+        <div className="flex justify-between items-center min-w-full py-5 p-8 xl:px-32">
+          <div className="bg-primary rounded-full p-2 shadow-sm">
+            <ChatBubbleOvalLeftIcon className="w-8 h-8 text-white" />
+          </div>
+
+          <div className="flex gap-x-8 lg:gap-x-16">
+            {navlist.map((item) => (
+              <Link
+                href={item.href}
+                target="_blank"
+                key={item.name}
+                className="font-medium text-gray-800 cursor-pointer text-[17px] hover:text-primary"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
+      </nav>
+      <section className="flex-[0.5] flex flex-col justify-center items-center z-10">
+        <div className="p-[30px]">
+          <h1 className="font-bold text-7xl mb-24 leading-[90px]">
+            <span className="text-gradient">Instant Messages</span>
+            <br />
+            <span className="text-gradient">Real Connections</span>
+          </h1>
+          <button
+            className="flex gap-2 items-center bg-white border border-gray-300 shadow-md rounded-xl py-3 px-4 text-gray-500 font-medium hover:bg-gray-50"
+            // onClick={() => {/* PENDING */}}
+          >
+            <Image src={GoogleIcon} alt="google" className="" width={24} height={24} />
+            Continue with Google
+          </button>
+        </div>
+      </section>
+      <section className="flex-[0.5] flex flex-col justify-center items-center z-10">
+        <div className="p-3 flex justify-between gap-4 items-center border rounded-xl border-gray-300 bg-white shadow-lg">
+          <Image
+            src="https://randomuser.me/api/portraits/women/2.jpg"
+            alt="random women"
+            className="rounded-[10px]"
+            width={40}
+            height={40}
+          />
+          <div className="flex flex-col">
+            <h4 className="text-gray-800 font-semibold text-lg">
+              Romina Ranolds
+            </h4>
+            <div className="flex flex-row justify-start items-center gap-1">
+              <div className="w-[8px] h-[8px] rounded-full bg-green-500"></div>
+              <p className="text-gray-500">Online</p>
+            </div>
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="p-3 mt-4 ml-24 flex justify-between gap-4 items-center border rounded-xl border-gray-300 bg-white shadow-lg">
+          <Image
+            src="https://randomuser.me/api/portraits/men/2.jpg"
+            alt="random women"
+            className="rounded-[10px]"
+            width={40}
+            height={40}
+          />
+          <div className="flex flex-col">
+            <h4 className="text-gray-800 font-semibold text-lg">
+              Aiden Parker
+            </h4>
+            <div className="flex flex-row justify-start items-center gap-1">
+              <div className="w-[8px] h-[8px] rounded-full bg-green-500"></div>
+              <p className="text-gray-500">Online</p>
+            </div>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="p-3 mt-4 mr-24 flex justify-between gap-4 items-center border rounded-xl border-gray-300 bg-white shadow-lg">
+          <Image
+            src="https://randomuser.me/api/portraits/women/7.jpg"
+            alt="random women"
+            className="rounded-[10px]"
+            width={40}
+            height={40}
+          />
+          <div className="flex flex-col">
+            <h4 className="text-gray-800 font-semibold text-lg">
+              Sophia Anderson
+            </h4>
+            <div className="flex flex-row justify-start items-center gap-1">
+              <div className="w-[8px] h-[8px] rounded-full bg-green-500"></div>
+              <p className="text-gray-500">Online</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-3 mt-6 mr-8 flex flex-col justify-center gap-1 items-start border border-message-received border-gray-300 bg-white shadow-lg">
+          <p className="text-gray-800 text-base font-medium">
+            I wish you good luck, keep going.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="text-gray-500 text-xs">
+            {currentHour}:{currentMinutes}
           </p>
-        </a>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+        <div className="p-3 mt-4 ml-8 flex flex-col justify-center gap-1 items-start border-message-send max-w-[270px] bg-primary shadow-lg">
+          <p className="text-white text-base font-medium leading-[20px]">
+            Thank you so much, I promise I will never give up.
           </p>
-        </a>
+          <p className="text-gray-100 text-xs">
+            {currentHour}:{currentMinutes}
+          </p>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="flex justify-center items-center gap-4 mt-12">
+          <div className="bg-primary/5 rounded-xl p-2 shadow-sm hover:scale-110 transition-[transform]">
+            <ChatBubbleOvalLeftIcon className="w-7 h-7 text-primary" />
+          </div>
+          <div className="bg-primary rounded-xl p-2 shadow-sm hover:scale-110 transition-[transform]">
+            <PaperAirplaneIcon className="w-7 h-7 text-white" />
+          </div>
+          <div className="bg-primary/5 rounded-xl p-2 shadow-sm hover:scale-110 transition-[transform]">
+            <CameraIcon className="w-7 h-7 text-primary" />
+          </div>
+          <div className="bg-primary/5 rounded-xl p-2 shadow-sm hover:scale-110 transition-[transform]">
+            <UsersIcon className="w-7 h-7 text-primary" />
+          </div>
+        </div>
+      </section>
     </main>
-  );
+  )
 }
